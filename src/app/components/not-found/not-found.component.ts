0770d55f61928687f3ac2590740e0b96ac09c5ff
Douglas <douglasevertonev@gmail.com>
2024-08-12 20:30:49 -0300

    feat: 404 página

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, RouterLink],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
