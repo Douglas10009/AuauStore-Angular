2f6cb7a47c1ecd1bcb82cf937491104b1ddafa72
Douglas <douglasevertonev@gmail.com>
2024-08-12 20:33:27 -0300

    fix: página 404

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [HeaderComponent, RouterLink],
+  imports: [HeaderComponent, RouterLink, FooterComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
