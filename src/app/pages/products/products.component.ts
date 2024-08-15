90fc74c05b24b0133ea6f103cda3332eb5dd7cda
Douglas <douglasevertonev@gmail.com>
2024-08-14 22:45:38 -0300

    feat: páginar de produtos

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -2,14 +2,30 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 import { CardProductsComponent } from "../../components/card-products/card-products.component";
+import { ProdutosCaninos } from '../../../IprodutosCaninos';
+import { CardProductsService } from '../../card-products.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, CardProductsComponent],
+  imports: [HeaderComponent, FooterComponent, CardProductsComponent, CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
 
+    // --------------------------
+    // CÓDIGO REUTILIZADO DO home.component.html
+    produtosCaninos!: ProdutosCaninos[]
+
+    constructor (private cardProductsService:CardProductsService) {}
+  
+    ngOnInit(){
+      this.cardProductsService.getProducts().subscribe((products) => {
+        this.produtosCaninos = products
+      })
+    }
+
+    // --------------------------
 }
