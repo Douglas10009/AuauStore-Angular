3cfd324f0c4d6b00199d5b261ecb53c3d5da7717
Douglas <douglasevertonev@gmail.com>
2024-08-14 22:25:32 -0300

    fix: social media

--- src/app/pages/social-medias/social-medias.component.ts
+++ src/app/pages/social-medias/social-medias.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { CardSocialMediaComponent } from '../../components/card-social-media/card-social-media.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-social-medias',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, CardSocialMediaComponent, CommonModule],
   templateUrl: './social-medias.component.html',
   styleUrl: './social-medias.component.scss'
 })
 export class SocialMediasComponent {
-
+  array = [1,2,3,4,5,6,7,8,9,10]
 }
