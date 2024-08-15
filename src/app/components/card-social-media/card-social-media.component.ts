7ba955aa7f28128f161279838a4dc2de50e9958d
Douglas <douglasevertonev@gmail.com>
2024-08-14 22:25:17 -0300

    feat: card social media

---
+++ src/app/components/card-social-media/card-social-media.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-social-media',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-social-media.component.html',
+  styleUrl: './card-social-media.component.scss'
+})
+export class CardSocialMediaComponent {
+
+}
