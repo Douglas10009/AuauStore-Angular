162da02c264fe4e4e0966ab60e320e575eb8e135
Douglas <douglasevertonev@gmail.com>
2024-08-14 22:38:27 -0300

    fix: Minimal fix

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+import { FooterComponent } from "../../components/footer/footer.component";
+import { CardProductsComponent } from "../../components/card-products/card-products.component";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, CardProductsComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
