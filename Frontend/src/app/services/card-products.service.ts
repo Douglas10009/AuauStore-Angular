c948f5955a845b4917033c579bf394e2bb61e0cf
Douglas <douglasevertonev@gmail.com>
2024-12-08 17:55:37 -0300

    fix: reorganizando

--- Frontend/src/app/services/card-products.service.ts
+++ Frontend/src/app/services/card-products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ProdutosCaninos } from '../../../IprodutosCaninos';
+import { ProdutosCaninos } from '../interface/IprodutosCaninos';
 
 
 @Injectable({
