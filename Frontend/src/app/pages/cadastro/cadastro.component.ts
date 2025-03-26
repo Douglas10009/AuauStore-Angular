78717874a28268a0e04c85d98a4dc3a587f0e45e
Douglas Éverton <70817854+Douglas10009@users.noreply.github.com>
2025-03-26 01:10:43 +0000

    feat: cadastro front

--- Frontend/src/app/pages/cadastro/cadastro.component.ts
+++ Frontend/src/app/pages/cadastro/cadastro.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-cadastro',
+  selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.scss']
 })
 export class CadastroComponent {
-   
+  @Input() localNome = "";
+  @Input() localEmail = "";
+  @Input() localSenha = "";
+
+  constructor(private router: Router) {}
+
+  cadastrar() {
+    // Colocar o service para adicionar ao banco de dados
+    alert(`Conta criada com sucesso para ${this.localNome} (${this.localEmail})!`);
+    this.router.navigate(['/login']);
+  }
 }
