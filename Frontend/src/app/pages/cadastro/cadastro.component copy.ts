b84ef422e14d1dc4cf645597080b20301159d50d
Douglas Éverton <70817854+Douglas10009@users.noreply.github.com>
2025-03-26 01:11:31 +0000

    minor changes

--- Frontend/src/app/pages/cadastro/cadastro.component copy.ts
+++ Frontend/src/app/pages/cadastro/cadastro.component copy.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-cadastro',
-  standalone: true,
-  imports: [],
-  templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.scss'
-})
-export class CadastroComponent {
+// @Component({
+//   selector: 'app-cadastro',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './cadastro.component.html',
+//   styleUrl: './cadastro.component.scss'
+// })
+// export class CadastroComponent {
 
-}
+// }
