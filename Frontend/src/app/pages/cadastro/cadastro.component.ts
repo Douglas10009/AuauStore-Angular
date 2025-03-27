38e9c489080cde90ba5f9f65b8838c6e69210a39
Douglas Éverton <70817854+Douglas10009@users.noreply.github.com>
2025-03-27 13:42:11 +0000

    fix: atualizar cadastro

--- Frontend/src/app/pages/cadastro/cadastro.component.ts
+++ Frontend/src/app/pages/cadastro/cadastro.component.ts
@@ -3,6 +3,8 @@ import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { CardProductsService } from '../../services/card-products.service';
+import { Cliente } from '../../interface/Icliente';
 
 @Component({
   selector: 'app-register',
@@ -16,11 +18,31 @@ export class CadastroComponent {
   @Input() localEmail = "";
   @Input() localSenha = "";
 
-  constructor(private router: Router) {}
+
+  cliente: Cliente = {
+    idCliente: 0,
+    nome: '',
+    idade: 0,
+    email: '',
+    senha: ''
+  };
+
+  constructor(private router: Router, private cardProductsService: CardProductsService) { }
 
   cadastrar() {
     // Colocar o service para adicionar ao banco de dados
     
+    this.cardProductsService.addClient(this.cliente).subscribe(
+      (response) => {
+        console.log('Cliente cadastrado com sucesso!', response);
+      },
+      (error) => {
+        console.error('Erro ao cadastrar cliente:', error);
+      }
+    );
+
+
+
     alert(`Conta criada com sucesso para ${this.localNome} (${this.localEmail})!`);
     this.router.navigate(['/login']);
   }
